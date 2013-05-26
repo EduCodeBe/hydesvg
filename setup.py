199b64ad3174db0e7a44c503a9f8da47f8107c01
Julien Pivotto <roidelapluie@inuits.eu>
2013-05-26 17:01:33 +0200

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='hydesvg',
-      version='0.1',
+      version='0.2',
       description='RMLL extensions for Hyde and Jinja2',
       author='Julien Pivotto',
       #long_description='',
